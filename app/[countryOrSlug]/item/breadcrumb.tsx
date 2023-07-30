1f01b2a8c30948625004d75ffd86f86c98e2f8ef
sinapirani <sinapiranidev@gmail.com>
2023-07-30 17:49:04 +0330

    Add country to item breadcrumb

--- app/[countryOrSlug]/item/breadcrumb.tsx
+++ app/[countryOrSlug]/item/breadcrumb.tsx
@@ -10,8 +10,9 @@ export const ItemBreadCrumb = ({pageData}: ItemBreadCrumbProps) => {
     <div className=" text-sm breadcrumbs">
       <ul>
         <li><Link href={"/"}>خانه</Link></li>
+        <li><Link href={`/${pageData?.country?.code}`}>{pageData?.country.name}</Link></li>
         <li><Link href={`/${pageData?.country?.code}/${pageData?.unit?.slug}`}>{pageData?.unit?.name}</Link></li>
-        <li><Link href={`/${pageData?.country?.code}/${pageData?.unit?.slug}?city=${pageData?.city?.id}&category=${pageData.category?.id}`}>{pageData?.category?.name}</Link></li>
+        <li><Link href={`/${pageData?.country?.code}/${pageData?.unit?.slug}/${pageData?.category.slug}`}>{pageData?.category?.name}</Link></li>
       </ul>
     </div>
   );
