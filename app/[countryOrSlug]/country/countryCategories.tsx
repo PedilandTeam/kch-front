ced123c74b0513f4ad9ed2d10cbaa587927bfc98
pediland <pediland.com@gmail.com>
2023-08-07 12:58:52 +0200

    Lastest Change Country

--- app/[countryOrSlug]/country/countryCategories.tsx
+++ app/[countryOrSlug]/country/countryCategories.tsx
@@ -39,16 +39,18 @@ export const CountryCategories = async ({
   // const units = await getUnits()
 
   return (
-    <div className="mod-categories my-8 sm:my-16">
-      <div className="mod-header pr-3 pl-3 mb-7 sm:pr-4 sm:pl-0 border-r-[4px] sm:border-r-[5px] border-pink-600">
-        <h2 className="text-[20px] sm:text-[24px] font-bold">
-          {_TXT.CATEGORY.UPDATED}:
-        </h2>
+    <div className="container mx-auto max-w-[1144px]">
+      <div className="mod-categories my-8 sm:my-16">
+        <div className="mod-header pr-3 pl-3 mb-7 sm:pr-4 sm:pl-0 border-r-[4px] sm:border-r-[5px] border-pink-600">
+          <h2 className="text-[20px] sm:text-[24px] font-bold">
+            {_TXT.CATEGORY.UPDATED}:
+          </h2>
+        </div>
+        <CountryCategoriesItems
+          currentCountry={currentCountry}
+          recentlyUpdatedCategories={mostUsedCategories}
+        />
       </div>
-      <CountryCategoriesItems
-        currentCountry={currentCountry}
-        recentlyUpdatedCategories={mostUsedCategories}
-      />
     </div>
   );
 };
