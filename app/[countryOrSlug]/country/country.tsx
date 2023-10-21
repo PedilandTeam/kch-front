c408e544fe3041e6578da9a5292011403328a04e
sinapirani <sinapiranidev@gmail.com>
2023-10-21 10:47:29 +0330

    delete /** @ts-expect-error server component */

--- app/[countryOrSlug]/country/country.tsx
+++ app/[countryOrSlug]/country/country.tsx
@@ -13,14 +13,10 @@ type CountryProps = {
 export default function Country({ currentCountry, categories }: CountryProps) {
   return (
     <div className="component page-country">
-      {/** @ts-expect-error server component */}
       <SliderHome currentCountry={currentCountry} />
       <UnitsBanner currentCountry={currentCountry} />
-      {/** @ts-expect-error server component */}
       <CountryStats currentCountry={currentCountry} />
-      {/** @ts-expect-error server component */}
       <CountryCategories currentCountry={currentCountry} />
-      {/** @ts-expect-error server component */}
       <SeoText currentCountry={currentCountry} />
     </div>
   );
