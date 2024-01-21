906ed2c6693660dfcd30f50b2bf3c2e5f88e1907
Sina Pirani <sinapiranidev@gmail.com>
2024-01-21 15:43:27 +0330

    debug: hidden overflow of input to dont overlap end/start content

--- components/daisy/input.tsx
+++ components/daisy/input.tsx
@@ -20,7 +20,7 @@ export default function Input({
 
     return (
         <div className={`form-control w-full max-w-full ${className}`}>
-            <div className={`input relative ${className} ${bordered ? 'input-bordered' : ''} ${isInvalid ? 'input-error' : ''}`}>
+            <div className={`input relative overflow-hidden ${className} ${bordered ? 'input-bordered' : ''} ${isInvalid ? 'input-error' : ''}`}>
                 {startContent && (
                     <span className={`absolute inset-y-0 left-0 top-0 bottom-0 px-3 flex items-center bg-white`}>
                         {startContent}
