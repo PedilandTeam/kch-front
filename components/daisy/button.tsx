2ff53c63b227ac6ebbdac2bc4d6f6f15b4765c25
Sina Pirani <sinapiranidev@gmail.com>
2024-01-21 15:38:51 +0330

    feat: add button component for daisy

---
+++ components/daisy/button.tsx
@@ -0,0 +1,22 @@
+
+interface Button extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    isLoading?: boolean;
+    loading?: 'ball' | 'dots' | 'ring' | 'bars' | 'spinner',
+    isDisabled?: boolean,
+}
+export default function Button({
+    className,
+    isLoading = false,
+    loading = 'spinner',
+    isDisabled = false,
+    ...buttonProps
+}: Button) {
+    return (
+        <button
+            className={`btn ${isDisabled ? 'btn-disabled' : ''} ${className}`}
+            {...buttonProps}
+        >
+            ثبت نام کاربر {isLoading ? <div className={`loading ${loading} loading- loading-spinner loading-ms`}></div> : ''}        
+        </button>
+    );
+}
