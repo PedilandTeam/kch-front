122f7ad48434e233937a7ffdf78c308158b5f7b8
Sina Pirani <sinapiranidev@gmail.com>
2024-01-22 18:40:56 +0330

    feat: end and start content support for option component

--- components/daisy/option.tsx
+++ components/daisy/option.tsx
@@ -1,25 +1,48 @@
 
-import { forwardRef } from 'react'
+import { forwardRef, useRef } from 'react'
 export interface Option extends React.LiHTMLAttributes<HTMLLIElement> {
 
     className?: string,
     children: React.ReactNode,
     value: string | number,
+    startContent?: React.ReactNode,
+    endContent?: React.ReactNode
 
 }
-export default forwardRef(function Option({
+export default function Option({
     className,
     children,
     value,
+    startContent,
+    endContent,
     ...optionProps
-}: Option, ref: React.Ref<HTMLSpanElement>) {
+}: Option) {
+
+    const ref = useRef<HTMLSpanElement>(null)
+
+    const handleClick = (e: React.MouseEvent<HTMLSpanElement>) => {
+        e.stopPropagation()
+        ref.current?.click()
+    }
+
     return (
+
         <span
             ref={ref}
-            className={`option hover:bg-gray-100 active:scale-95 duration-75 select-none rounded-md py-3 px-1 ${className}`}
+            className={`option relative hover:bg-gray-100 active:scale-95 duration-75 select-none rounded-md py-3 px-1 ${className}`}
             {...optionProps}
         >
+            {startContent && (
+                <span onClick={handleClick} className={` px-3 flex items-center`}>
+                    {startContent}
+                </span>
+            )}
+            {endContent && (
+                <span  onClick={handleClick} className={` px-3 flex items-center `}>
+                    {endContent}
+                </span>
+            )}
             {value}
         </span>
     );
-})
+}
