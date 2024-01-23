9d6387cbd45a26d23071dfdbed6dd76ec4bbc99e
Sina Pirani <sinapiranidev@gmail.com>
2024-01-23 13:33:34 +0330

    debug: empty if dependency not selected & add isInvalid because we need that extra of nextui

--- components/daisy/selectWithFetching.tsx
+++ components/daisy/selectWithFetching.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { fetcher } from "@/app/swr/fetcher";
-import { memo, startTransition } from "react";
+import { memo, startTransition, useEffect } from "react";
 import useSWR from "swr";
 import { CircleFlag } from "next-circle-flags";
 import Select from "@daisyComponents/select";
@@ -22,7 +22,7 @@ type SelectWithFetching = {
 }
 export default memo(function SelectWithFetching<T extends { items: any }>({ route, label, paginated = false, name, isDisabled = false, circleFlag = false, formErrors, setFieldValue, className , value, bordered}: SelectWithFetching) {
 
-  const { data, isLoading, error } = useSWR<T>(`${process.env.NEXT_PUBLIC_API_URL}${route}`, fetcher)
+  const { data, isLoading, error } = useSWR<T>(route ? `${process.env.NEXT_PUBLIC_API_URL}${route}` : null, fetcher)
 
   return (
     <Select
@@ -33,13 +33,14 @@ export default memo(function SelectWithFetching<T extends { items: any }>({ rout
       isLoading={isLoading}
       isDisabled={isDisabled}
       errorMessage={formErrors![name]}
+      isInvalid={!!formErrors![name] ? true : false}
       label={label}
       value={value}
       bordered={bordered}
     >
       {
         (item: any) => (
-          <Option key={item.id} onClick={() => setFieldValue('countryId', item.id)} value={item.id} startContent={circleFlag ? <CircleFlag countryCode={item.code} height={20} width={20} alt={`پرچم کشور ${item.name}`}/> : null}>{item.name}</Option>
+          <Option key={item.id} value={item.id} startContent={circleFlag ? <CircleFlag countryCode={item.code} height={20} width={20} alt={`پرچم کشور ${item.name}`}/> : null}>{item.name}</Option>
         )
       }
     </Select>
