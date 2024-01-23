d68ac189f18d93b7c5f20e15ff6b3583e86194dc
Sina Pirani <sinapiranidev@gmail.com>
2024-01-23 12:55:32 +0330

    feat: create selectWIthFetching with fetch Component

---
+++ components/daisy/selectWithFetching.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { fetcher } from "@/app/swr/fetcher";
+import { memo, startTransition } from "react";
+import useSWR from "swr";
+import { CircleFlag } from "next-circle-flags";
+import Select from "@daisyComponents/select";
+import Option from '@daisyComponents/option'
+
+
+type SelectWithFetching = {
+  route: string;
+  label: string;
+  paginated?: boolean;
+  name: string;
+  isDisabled?: boolean;
+  circleFlag?: boolean;
+  formErrors: any;
+  setFieldValue: (name: string, value: any) => void;
+  className?: string;
+  value?: any;
+  bordered?: boolean
+}
+export default memo(function SelectWithFetching<T extends { items: any }>({ route, label, paginated = false, name, isDisabled = false, circleFlag = false, formErrors, setFieldValue, className , value, bordered}: SelectWithFetching) {
+
+  const { data, isLoading, error } = useSWR<T>(`${process.env.NEXT_PUBLIC_API_URL}${route}`, fetcher)
+
+  return (
+    <Select
+      className={className}
+      dir="ltr"
+      items={data ? (paginated ? data?.items : data) : []}
+      onChange={(e, value) => setFieldValue(name, value)}
+      isLoading={isLoading}
+      isDisabled={isDisabled}
+      errorMessage={formErrors![name]}
+      label={label}
+      value={value}
+      bordered={bordered}
+    >
+      {
+        (item: any) => (
+          <Option key={item.id} onClick={() => setFieldValue('countryId', item.id)} value={item.id} startContent={circleFlag ? <CircleFlag countryCode={item.code} height={20} width={20} alt={`پرچم کشور ${item.name}`}/> : null}>{item.name}</Option>
+        )
+      }
+    </Select>
+  )
+
+})
