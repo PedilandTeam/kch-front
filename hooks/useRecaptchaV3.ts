d8d21f056197a50d6d42943c1a5ed46a7ad6df89
Sina Pirani <sinapiranidev@gmail.com>
2024-01-24 14:33:05 +0330

    remove: don't load recaptcha script in useRecaptchaV3 and don't return token by state

--- hooks/useRecaptchaV3.ts
+++ hooks/useRecaptchaV3.ts
@@ -1,35 +1,13 @@
-import { useState, useEffect } from 'react';
-
-function useRecaptchaV3() {
-  const [token, setToken] = useState<string>('');
-
-  useEffect(() => {
-    // Check if reCAPTCHA script is already loaded
-    if (!(window as any).grecaptcha || !(window as any).grecaptcha.ready) {
-      // Dynamically insert script to page
-      const script = document.createElement('script');
-      script.src = `https://www.google.com/recaptcha/api.js?render=YOUR_SITE_KEY`;  
-      script.async = true;
-      script.defer = true;
-      document.body.appendChild(script);
-    }
-    return () => {
-      // Cleanup
-    };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [window]);
-  
+function useRecaptchaV3() {  
   const executeRecaptcha = async (action: string) => {
     return await (window as any).grecaptcha
       .execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V3!, {action: action}) 
       .then((token: string) => {
-        setToken(token);
         return token;
       });
   };
 
   return {
-    token,
     executeRecaptcha
   };
 
