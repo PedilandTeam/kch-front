875fac974aa039587cbacf82d208c53caaa82c0b
Sina Pirani <sinapiranidev@gmail.com>
2024-01-24 13:57:08 +0330

    feat: generate own recaptchaProvider and return token for generate token

--- hooks/useRecaptchaV3.ts
+++ hooks/useRecaptchaV3.ts
@@ -5,7 +5,7 @@ function useRecaptchaV3() {
 
   useEffect(() => {
     // Check if reCAPTCHA script is already loaded
-    if (!window.grecaptcha || !window.grecaptcha.ready) {
+    if (!(window as any).grecaptcha || !(window as any).grecaptcha.ready) {
       // Dynamically insert script to page
       const script = document.createElement('script');
       script.src = `https://www.google.com/recaptcha/api.js?render=YOUR_SITE_KEY`;  
@@ -16,12 +16,13 @@ function useRecaptchaV3() {
     return () => {
       // Cleanup
     };
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [window]);
   
-  const executeRecaptcha = (action: string) => {
-    return window.grecaptcha
+  const executeRecaptcha = async (action: string) => {
+    return await (window as any).grecaptcha
       .execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V3!, {action: action}) 
-      .then(token => {
+      .then((token: string) => {
         setToken(token);
         return token;
       });
