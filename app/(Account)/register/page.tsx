b3cfca28280bf23a189eced0a8bbec503116fd42
Sina Pirani <sinapiranidev@gmail.com>
2024-01-24 14:32:11 +0330

    feat: prevent from authenticated user to see register form

--- app/(Account)/register/page.tsx
+++ app/(Account)/register/page.tsx
@@ -1,34 +1,34 @@
+
 import RegisterForm from "./form";
 import { API_ROUTES } from "@/routes";
+import axios from "axios";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { redirect } from "next/navigation"; 
 
 const Register = async (param?: {
   searchParams?: { slug?: string; claimWay: string };
 }) => {
-  const slug = param?.searchParams?.slug;
-  const claimWay = param?.searchParams?.claimWay;
-
   const cookieStore = cookies();
   const token = cookieStore.get("token")?.value;
   let haveAccess: boolean = true;
+
   if (token) {
-    await API_ROUTES.AUTH.CHECK(token)
-      .then(async (res) => {
-        const data = await res.json();
-        haveAccess = false;
-      })
-      .catch((e) => {
-        if (e?.status == 401) {
-          haveAccess = true;
-        }
-        console.log(e);
-      });
+    await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/user/check`, {
+      headers: {
+        Cookie: `token=${token}`
+      }
+    })
+    .then(res => {
+      haveAccess = false;
+    })
+    .catch(e => {
+      console.log(e);
+    })
   }
 
   if (!haveAccess) {
-    redirect("/home/mybiz/add");
+    redirect("/account");
   }
-  return <RegisterForm slug={slug} claimWay={claimWay} />;
+  return <RegisterForm />;
 };
 export default Register;
