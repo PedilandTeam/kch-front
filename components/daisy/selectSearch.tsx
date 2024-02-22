ad33a97e288c65a3f009c21931a639af1fb1186e
Sina Pirani <sinapiranidev@gmail.com>
2024-02-22 15:43:02 +0330

    fat: Refactor SelectSearch component to delay search execution
    Introduce a delay in updating the search value to enhance user experience by reducing unnecessary search calls. Incorporate the useEffect hook to set a timeout before updating the search. This helps prevent constantly updating the search while the user is typing

--- components/daisy/selectSearch.tsx
+++ components/daisy/selectSearch.tsx
@@ -1,15 +1,25 @@
 'use client';
 
-import { Dispatch, SetStateAction, memo, useState } from 'react';
+import { Dispatch, SetStateAction, memo, useEffect, useState } from 'react';
 
 export interface SelectSearch {
     setSearch: Dispatch<SetStateAction<string>>;
 }
 function SelectSearch({ setSearch }: SelectSearch) {
 
+    const [tempSearch, setTempSearch] = useState<string>('')
+
     const onSearchChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
+        setTempSearch(e.target.value);
     }
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+          setSearch(tempSearch);
+        }, 1000);
+        return () => clearTimeout(timeout);
+      }, [tempSearch, setSearch]);
+    
 
     return (
         <div className='sticky top-0 z-10 w-full bg-white p-2'>
