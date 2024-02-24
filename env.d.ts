87ddf3d200618b281408554b82a66fd1021875da
Sina Pirani <sinapiranidev@gmail.com>
2024-02-24 12:46:23 +0330

    Add NEXT_PUBLIC_HERE_API_URL and reorganize env.d.ts
    Updated .env to include NEXT_PUBLIC_HERE_API_KEY and NEXT_PUBLIC_CHECKAUTH_URL. Updated env.d.ts to declare the new environment variables. This change prepares the environment for integrating location-based services and enhances authentication.

--- env.d.ts
+++ env.d.ts
@@ -1,24 +1,27 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            API_KEY: string;
-            API_URL: string;
-            FRONT_URL: string;
-            NEXT_PUBLIC_API_URL: string;
-            NEXT_PUBLIC_DL_URL: string;
-            NEXT_PUBLIC_FRONT_URL: string;
-            NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V2: string;
-            NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V3: string;
-            NOTIFER_KEY: string;
-            NOTIFER_URL: string;
-            PORT: string;
-            RECAPTCHA_SECRET_KEY: string;
-            REDIS_URL: string;
-            REVALIDATE_DEFAULT: string;
-            TEL_API_URL: string;
-            TEL_BOT_TOKEN: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      API_KEY: string;
+      API_URL: string;
+      FRONT_URL: string;
+      NEXT_PUBLIC_API_URL: string;
+      NEXT_PUBLIC_DL_URL: string;
+      NEXT_PUBLIC_FRONT_URL: string;
+      NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V2: string;
+      NEXT_PUBLIC_RECAPTCHA_SITE_KEY_V3: string;
+      NOTIFER_KEY: string;
+      NOTIFER_URL: string;
+      PORT: string;
+      RECAPTCHA_SECRET_KEY: string;
+      REDIS_URL: string;
+      REVALIDATE_DEFAULT: string;
+      TEL_API_URL: string;
+      TEL_BOT_TOKEN: string;
+      NEXT_PUBLIC_HERE_API_URL: string;
+      NEXT_PUBLIC_HERE_API_KEY: string;
+      NEXT_PUBLIC_CHECKAUTH_URL: string;
     }
+  }
 }
 
 export {};
