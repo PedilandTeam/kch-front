1bc907f9c8c04270e598dd3c1b539fac41a5ac5b
Sina Pirani <sinapiranidev@gmail.com>
2024-02-26 11:54:11 +0330

    Refactor deletePictureHandler to use useState for loading state
    Use useState to manage the loading state in deletePictureHandler for better reactivity and control. Use the loading state to conditionally apply a CSS class for visual feedback during the delete operation.

--- app/(Account)/account/ads/components/pictureWithLink.tsx
+++ app/(Account)/account/ads/components/pictureWithLink.tsx
@@ -1,12 +1,17 @@
 import useAdPicture from "@/store/useAdPicture";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import axios, { AxiosError } from "axios";
+import { useState } from "react";
 import toast from "react-hot-toast";
 import { mutate } from "swr";
 
 export default function PictureWithLink({link, adId, pictureId }: {link?: string, adId: string, pictureId: string}) {
 
+
+    const [deleteLoading, setDeleteLoading] = useState<boolean>(false)
+
     const deletePictureHandler = async() => {
+        setDeleteLoading(true)
         toast.promise(
             axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/ads/${adId}/pictures/${pictureId}`, {
                 withCredentials: true,
@@ -17,6 +22,9 @@ export default function PictureWithLink({link, adId, pictureId }: {link?: string
             .catch((e: AxiosError) => {
                 const data = e.response?.data as {message: string}
                 toast.error(`خطایی رخ داد ${data?.message}`)
+            })
+            .finally(() => {
+                setDeleteLoading(false)
             }),
             {
                 loading: 'درحال حذف عکس',
@@ -28,7 +36,7 @@ export default function PictureWithLink({link, adId, pictureId }: {link?: string
 
     return (
         <div
-            className='relative h-28 w-28 overflow-hidden rounded-xl'
+            className={`relative h-28 w-28 overflow-hidden rounded-xl ${deleteLoading && 'transform-cpu animate-pulse'} `}
         >
             <div onClick={deletePictureHandler} className='absolute left-2 top-2 rounded-lg bg-black/40 p-1 text-white active:scale-95'>
                 <TrashIcon className='h-4 w-4' />
