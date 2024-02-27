429ed2559b610818a9b0504d80a53ea5f568028b
Sina Pirani <sinapiranidev@gmail.com>
2024-02-27 17:49:34 +0330

    Refactor picture uploader to handle multiple file uploads
    Refactor the picture uploader component to handle multiple file uploads at once, improving user experience and ease of adding pictures. This change enhances the functionality of the picture uploader component. No associated issues.

--- components/global/pictureUploader/pictureUploader.tsx
+++ components/global/pictureUploader/pictureUploader.tsx
@@ -4,28 +4,44 @@ import { PlusIcon } from '@heroicons/react/24/solid';
 import { useEffect } from 'react';
 
 export default function PictureUploader() {
-
-    const { addPicture, pictures } = useAdPicture()
+    const { addPicture, pictures } = useAdPicture();
 
     const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const fileReader = new FileReader()
         if (!e.target.files) return;
-        const file = e.target.files[0];
-        if(!file) return;
-        fileReader.readAsBinaryString(file);
-        fileReader.onload = (event: ProgressEvent<FileReader>) => {
-            const blob = new Blob([event.target?.result as BlobPart], { type: file.type })
-            addPicture(blob, file)
-            e.target.value = ''
-        }
+
+        const files = Array.from(e.target.files); // Accept multiple files
+        if (files.length === 0) return;
+
+        files.forEach((file) => {
+            const fileReader = new FileReader();
+            fileReader.readAsBinaryString(file);
+            fileReader.onload = (event: ProgressEvent<FileReader>) => {
+                const blob = new Blob([event.target?.result as BlobPart], {
+                    type: file.type,
+                });
+                addPicture(blob, file);
+            };
+        });
+
+        e.target.value = ''; // Clear input value after processing files
     };
 
     return (
         <>
-            <label htmlFor='newpic' className='flex h-28 w-28 items-center justify-center rounded-xl border-2 border-dotted'>
+            <label
+                htmlFor='newpic'
+                className='flex h-28 w-28 items-center justify-center rounded-xl border-2 border-dotted'
+            >
                 <PlusIcon className='h-12 w-12 text-primary' />
             </label>
-            <input onChange={onFileChange} accept='.jpg, .jpeg, .png, .webp' id='newpic' type="file" className='hidden' />
+            <input
+                onChange={onFileChange}
+                accept='.jpg, .jpeg, .png, .webp'
+                id='newpic'
+                type='file'
+                className='hidden'
+                multiple
+            />
         </>
     );
 }
