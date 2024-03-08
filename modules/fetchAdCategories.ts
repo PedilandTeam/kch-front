6ca21340400e07cc89897dfa796eb43e61d51afb
Sina Pirani <sinapiranidev@gmail.com>
2024-03-08 19:12:41 +0330

    Refactor ad category and ad retrieval functions
    Refactor ad category and ad retrieval functions to utilize a consistent filters object, improve readability, and handle potential errors more effectively. By standardizing the filters input, the code becomes more maintainable, and error logging now includes comprehensive details. These changes aim to enhance code organization and error handling.

--- modules/fetchAdCategories.ts
+++ modules/fetchAdCategories.ts
@@ -1,30 +1,59 @@
-import { AdCategoryNamepace } from "@/types/adCategory";
-import { CategoryNamespace } from "@/types/category";
+import { AdCategoryNamepace } from '@/types/adCategory';
+import { CategoryNamespace } from '@/types/category';
 import wretch from 'wretch';
-import { WretchError } from "wretch/resolver";
+import { WretchError } from 'wretch/resolver';
 
+export default async function fetchAdCategories({
+    justMain,
+    parentId,
+    parentSlug,
+    search,
+    revalidate,
+    slug,
+}: {
+    justMain?: boolean;
+    parentId?: number | string;
+    parentSlug?: string;
+    search?: string;
+    revalidate?: number;
+    slug?: string;
+}): Promise<AdCategoryNamepace.IAdCategory[]> {
+    const urlObject = new URL(`${process.env.API_URL}/ad-categories`);
 
-export default async function fetchAdCategories({ justMain, parentId, search, revalidate }: { justMain?: boolean, parentId?: number, search?: string, revalidate?: number }): Promise<AdCategoryNamepace.IAdCategory[]> {
-    const urlObject = new URL(`${process.env.API_URL}/ad-categories`)
+    if (parentId && parentSlug) {
+        console.error(
+            `Error Accourd in fetchAdCategory. params {parentId: ${parentId}, parentSlug: ${parentSlug}}`
+        );
+        throw Error(
+            `Error Accourd in fetchAdCategory. params {parentId: ${parentId}, parentSlug: ${parentSlug}}`
+        );
+    }
 
-    if (justMain) urlObject.searchParams.append('justMain', 'true')
-    if (parentId) urlObject.searchParams.append('parentId', parentId.toString())
-    if (search) urlObject.searchParams.append('search', search)
+    if (justMain) urlObject.searchParams.append('justMain', 'true');
+    if (parentId)
+        urlObject.searchParams.append('parentId', parentId.toString());
+    if (search) urlObject.searchParams.append('search', search);
+    if (slug) urlObject.searchParams.append('slug', slug);
+    if (parentSlug) urlObject.searchParams.append('parentSlug', parentSlug);
 
-    const url = urlObject.toString()
+    const url = urlObject.toString();
     const OPTIONS = {
         next: {
-            revalidate
-        }
-    }
+            revalidate,
+        },
+    };
     return await wretch(url)
         .options(OPTIONS)
         .get()
-        .json(json => {
-            return json
+        .json((json) => {
+            return json;
         })
         .catch((e: WretchError) => {
-            console.error(`Error Accourd in fetchAdCategory. params {justMain: ${justMain}, parentId: ${parentId}, search: ${search}}`, e.json, e.message)
+            console.error(
+                `Error Accourd in fetchAdCategory. params {justMain: ${justMain}, parentId: ${parentId}, search: ${search}}`,
+                e.json,
+                e.message
+            );
             return null;
-        })
+        });
 }
--- modules/fetchAds.ts
+++ modules/fetchAds.ts
@@ -1,35 +1,49 @@
-import { AdNamespace } from "@/types/ad";
-import { AdCategoryNamepace } from "@/types/adCategory";
-import { CategoryNamespace } from "@/types/category";
+import { AdNamespace } from '@/types/ad';
+import { AdCategoryNamepace } from '@/types/adCategory';
+import { CategoryNamespace } from '@/types/category';
 import wretch from 'wretch';
-import { WretchError } from "wretch/resolver";
+import { WretchError } from 'wretch/resolver';
 
+interface Filters {
+    [key: string]: any;
+    page?: number;
+    limit?: number;
+    coutnryCode?: string;
+    cityIds?: number[];
+    revalidate?: number;
+    categoryId?: number | string;
+    categorySlug?: string;
+    search?: string
+}
+export default async function fetchAds(filters: Filters): Promise<AdNamespace.GET> {
+    const urlObject = new URL(`${process.env.API_URL}/ads`);
 
-export default async function fetchAds({ page=1, limit= 30, coutnryCode, cityIds, revalidate }: { page?: number, limit?: number, coutnryCode?: string, cityIds?: number[], revalidate?: number }): Promise<AdNamespace.GET> {
-    const urlObject = new URL(`${process.env.API_URL}/ads`)
+    Object.keys(filters).forEach(filter => {
+        const value = filters[filter];
+        if (value) urlObject.searchParams.append(filter, String(value));
+    })
 
-    if (page) urlObject.searchParams.append('page', String(page))
-    if (limit) urlObject.searchParams.append('limit', String(limit))
-    if (coutnryCode) urlObject.searchParams.append('countryCode', String(coutnryCode))
-    if (cityIds) urlObject.searchParams.append('cityIds', String(cityIds))
+    const url = urlObject.toString();
 
-    const url = urlObject.toString()
-    const OPTIONS = {
-        next: {
-            revalidate
-        }
-    }
+    const { revalidate } = filters
+    const OPTIONS = revalidate ? { next: {revalidate}} : {}
+
     return await wretch(url)
         .options(OPTIONS)
         .get()
-        .json(json => {
-            return json
+        .json((json) => {
+            return json;
         })
         .catch((e: WretchError) => {
-            console.error(`Error Accourd in fetchAds. params {page: ${page}, limit: ${limit}, coutnryCode: ${coutnryCode}, cityIds: ${cityIds}}`, e.json, e.message)
+            console.error(
+                `Error Accourd in fetchAds. params:`,
+                filters,
+                e.json,
+                e.message
+            );
             if (e.json) {
                 return e.json;
             }
-            return null
-        })
+            return null;
+        });
 }
