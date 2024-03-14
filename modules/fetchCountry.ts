ec302132c4df0fbaeb161a5c7a4eebe21fcd848d
Sina Pirani <sinapiranidev@gmail.com>
2024-03-14 19:04:19 +0330

    Refactor fetchCountry to accept optional filter parameters
    Modify the fetchCountry function to accept optional filter parameters, enhancing its flexibility and usability for different data retrieval scenarios. This change simplifies the usage of the fetchCountry function by allowing certain parameters to be optional, which can be beneficial for certain use cases. (Issue #123)

--- modules/fetchCountry.ts
+++ modules/fetchCountry.ts
@@ -2,7 +2,7 @@ import { CountryNamespace } from "@/types/country";
 import fetchWrapper from "./fetchWrapper";
 
 
-export type FetchCountry = { code: string, revalidate: number }
-export default async function fetchCountry(filters: FetchCountry, revalidate?: number): Promise<CountryNamespace.GET[]> {
+export type FetchCountry = { code?: string }
+export default async function fetchCountry(filters?: FetchCountry, revalidate?: number): Promise<CountryNamespace.GET[]> {
     return await fetchWrapper('/countries', { filters: filters, isPaginated: false, revalidate: revalidate });
 }
