cb7238312009122a62a60eaa43b6d06bc48baa90
Sina Pirani <sinapiranidev@gmail.com>
2024-03-17 16:25:43 +0330

    fix: homepage canonical

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: "کوچا | جامعه ایرانیان مهاجر مقیم همه جا",
   description:
     "هر جای دنیا که زندگی می کنید، کوچا همراه شماست تا بتوانید نیازهای خود را به زبان مادری و به سادگی رفع کنید. از معرفی مشاغل محلی گرفته تا پزشکانی که به زبان فارسی صحبت می کنند.",
+  alternates: {
+    canonical: process.env.FRONT_URL,
+  }
 };
 export default function Page() {
   return (
