bebc2ccda58458089afb93ca87167bef319773f4
Sina Pirani <sinapiranidev@gmail.com>
2024-06-09 20:32:30 +0330

    Feat: log header

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,10 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  for (const key in request.headers.keys()) {
-    console.log(`${key}: ${request.headers.get(key)}`);
-  }
+    request.headers.forEach((value, key) => {
+        console.log(key, value);
+        
+    })
   return NextResponse.next()
   
 }
