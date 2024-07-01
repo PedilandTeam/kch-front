aaa478734c87537f936729ea45320b4652872fa9
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-07-01 23:01:45 +0330

    logo added to bottomHeader

--- app/(Site)/[countryOrSlug]/(Community)/community/bottomMenu.tsx
+++ app/(Site)/[countryOrSlug]/(Community)/community/bottomMenu.tsx
@@ -1,13 +1,23 @@
-"use client"
-import { Flag, HouseLine, List } from '@phosphor-icons/react'
-import React from 'react'
+"use client";
+import { Flag, HouseLine, List } from "@phosphor-icons/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function BottomMenu() {
   return (
     <div className="md:hidden fixed z-50 bottom-0 h-[5rem] w-full gap-[3rem] bg-white flex justify-center items-center py-2 border-t-2 border-gray-300">
-      <List size={25} color="#282828" />
-      <HouseLine size={25} color="#282828" />
-      <Flag size={25} color="#282828" />
+      <List size={32} color="#282828" />
+      <Link href="/">
+        <Image
+          src="/images/logo-01.png"
+          width={52}
+          height={39}
+          priority={true}
+          alt="Koochaa Logo"
+        />
+      </Link>
+      <Flag size={32} color="#282828" />
     </div>
-  )
+  );
 }
--- app/(Site)/[countryOrSlug]/(Community)/community/page.tsx
+++ app/(Site)/[countryOrSlug]/(Community)/community/page.tsx
@@ -15,9 +15,11 @@ export default function Page() {
   const timeOfQuestion: string = "22 دقیقه قبل";
   const UserQuestion: string = " آیا وبفلو مناسب برای ساخت وبسایت است؟";
   const questionDescription: string =
-    "    وبفلو یک ابزار پیشرفته برای ساخت وبسایت است که به شما امکان می دهد بدون نیاز به کدنویسی حرفه ای وبسایت های زیبا بسازید.";
+    "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده ";
   const topic = "مهاجرت";
   const countOfAnswer: string = "3 پاسخ";
+  const userBio: string = "ساکن المان از سال 2014";
+
   return (
     <div className="h-auto min-h-[53vh] flex flex-col items-center pb-[5rem]">
       <div className="fixed flex justify-center top-0 w-full z-50">
@@ -38,7 +40,7 @@ export default function Page() {
                 />
                 <div className="flex flex-col">
                   <span className="ml-2 font-bold">{userName} </span>
-                  <span className=" flex text-sm ">test</span>
+                  <span className=" flex text-sm ">{userBio}</span>
                 </div>
               </div>
             </div>
@@ -53,16 +55,16 @@ export default function Page() {
             </div>
           </div>
           {/* Topic and Up Vote */}
-          <div className="mt-5 flex justify-between items-center">
+          <div className="mt-5 flex text-sm justify-between items-center">
             <div className="text-sm text-gray-600">
               <span className="flex gap-1 font-bold ">
                 <Tag size={22} color="#262526" weight="duotone" />
-                <span> {topic}</span>
+                <span className="text-[.7rem]"> {topic}</span>
               </span>
             </div>
             <div className="flex gap-2 items-center">
               <div>
-                <div className="flex gap-1 text-sm">
+                <div className="flex gap-1 text-[.8rem]">
                   <span>
                     <ChatCircleDots
                       size={25}
@@ -73,17 +75,17 @@ export default function Page() {
                   {countOfAnswer}
                 </div>
               </div>
-              <div className="font-bold  w-auto p-1  text-gray-700  gap-2 h-[1.9rem] items-center flex rounded-xl text-xs bg-[#D9D9D9]">
+              <div className="font-bold  w-auto   text-gray-700  gap-2 h-[1.9rem] items-center flex rounded-xl text-xs bg-[#D9D9D9]">
                 <Button isIconOnly size="sm" variant="light">
                   <span className="">
                     <ArrowFatDown size={18} color="#da3316" weight="duotone" />
-                  </span>|
+                  </span>
                 </Button>
-
+                |
                 <Button size="sm" variant="light">
                   <span> باهات موافقم</span>
                   <span>
-                    <ArrowFatUp size={18} color="#87f471" weight="duotone" />
+                    <ArrowFatUp size={18} color="#2ed90c" weight="duotone" />
                   </span>
                 </Button>
               </div>
