186642c9d76b4240783a58a6e62979c79ef31273
Pedram Ghane <p.ghane@gmail.com>
2025-07-25 17:13:22 +0200

    Update: All packages updated to latest minor and project run

--- env.d.ts
+++ env.d.ts
@@ -2,24 +2,31 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT: string;
+      FRONT_URL: string;
+      NEXT_PUBLIC_FRONT_URL: string;
+      NEXT_PUBLIC_BIZ_FRONT_URL: string;
+
       API_URL: string;
-      TEL_BOT_TOKEN: string;
-      TEL_API_URL: string;
       NEXT_PUBLIC_API_URL: string;
       NEXT_PUBLIC_DL_URL: string;
-      NEXT_PUBLIC_FRONT_URL: string;
+
+      TEL_BOT_TOKEN: string;
+      TEL_API_URL: string;
+
       NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
       RECAPTCHA_SECRET_KEY: string;
+
       NOTIFER_URL: string;
       NOTIFER_KEY: string;
-      REVALIDATE_DEFAULT: string;
-      REDIS_URL: string;
+
       API_KEY: string;
-      FRONT_URL: string;
       NEXT_PUBLIC_DAYS_AS_NEW: string;
-      NEXT_PUBLIC_BIZ_FRONT_URL: string;
+
+      REVALIDATE_DEFAULT: string;
       REVALIDATE_SECRET: string;
       DEFAULT_REVALIDATE_TIME_FOR_PAGE_HANDLERS: string;
+
+      REDIS_URL: string;
     }
   }
 }
