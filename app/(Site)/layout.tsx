17821afe8cadf6d5baa483471c370f73a5cf62be
Pedram Ghane <p.ghane@gmail.com>
2025-07-30 19:03:05 +0200

    merge by fateme

--- app/(Site)/layout.tsx
+++ app/(Site)/layout.tsx
@@ -3,13 +3,14 @@
 import { Footer } from "@/app/(Site)/layout/footer";
 import { Header } from "@/app/(Site)/layout/header";
 import { ModalCountry } from "@/app/(Site)/layout/modalcountry";
+import Hotjar from "@/components/hotjar";
 import Fonts from "@/config/fonts";
 import { API_ROUTES } from "@/routes";
 import "@/styles/globals.css";
 import { CountryNamespace } from "@/types/country";
 import Script from "next/script";
 import { Toaster } from "react-hot-toast";
-import Hotjar from "@/components/hotjar";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default async function RootLayout({
   children,
@@ -27,6 +28,7 @@ export default async function RootLayout({
   return (
     <html lang="fa" dir="rtl" className="scroll-smooth">
       <body className="min-h-screen overflow-x-hidden">
+        <NextUIProvider>
         <Fonts />
         <Header countries={countries}>
           <ModalCountry countries={countries} />
@@ -48,6 +50,7 @@ export default async function RootLayout({
           gtag('config', 'G-EED4RG3GPD');
         `}
           </Script>
+        </NextUIProvider>
       </body>
     </html>
   );
