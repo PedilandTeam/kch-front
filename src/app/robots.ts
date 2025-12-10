c4ad7e7b982446677b61eeb2312335b24ca2dcca
Pedram Ghane <p.ghane@gmail.com>
2025-12-10 12:54:50 +0100

    Fix Robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,11 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/_next/", "/images/"],
+      disallow: ["/images/"],
     },
     sitemap: "https://koochaa.com/sitemap.xml",
   };
